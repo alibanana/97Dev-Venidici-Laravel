519638a790c6d7a1b3c479a26b1e5a13d9460bf7
Alifio Rasyid <official.alifio@gmail.com>
2021-04-25 14:04:55 +0700

    Added intervention/image package, added Helper class.
    Notes:
     - Inside the helper class, a simple storeImage function has been created.

--- config/app.php
+++ config/app.php
@@ -175,6 +175,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Additional Service Providers...
+         */
+        Intervention\Image\ImageServiceProvider::class
     ],
 
     /*
@@ -228,6 +232,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Helper' => App\Helper\Helper::class,
+        'Image' => Intervention\Image\Facades\Image::class,
     ],
 
 ];
