3c38b5e55099308e56013b5d127dcbd158c7746a
Reinhart Christian <reinhartchristian17@gmail.com>
2021-06-05 00:10:51 +0700

    collaborator done minus several routes

---
+++ database/migrations/2021_06_04_121121_create_collaborators_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCollaboratorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('collaborators', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('institution');
+            $table->string('institution_socmed');
+            $table->enum('collaborator_partnership', ['Event Collaboration', 'Brand Activation', 'Content Collaboration', 'Other']);
+            $table->string('email');
+            $table->integer('whatsapp');
+            $table->text('notes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('collaborators');
+    }
+}
