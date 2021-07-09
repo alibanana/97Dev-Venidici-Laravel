0a281ca382f2f3c322ac30dfa121c59e40b2705a
Reinhart Christian <reinhartchristian17@gmail.com>
2021-07-09 23:47:43 +0700

    store bootcamp done

--- app/Http/Controllers/Admin/BootcampController.php
+++ app/Http/Controllers/Admin/BootcampController.php
@@ -4,6 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Helper\Helper;
+use App\Helper\CourseHelper;
+
+use App\Models\CourseCategory;
+use App\Models\CourseType;
+use App\Models\Course;
+use App\Models\CourseRequirement;
+use App\Models\CourseFeature;
 
 class BootcampController extends Controller
 {
@@ -25,7 +34,9 @@ class BootcampController extends Controller
      */
     public function create()
     {
-        return view('admin/bootcamp/create');
+        $course_categories = CourseCategory::select('id', 'category')->get();
+
+        return view('admin/bootcamp/create', compact('course_categories'));
 
     }
 
@@ -37,7 +48,40 @@ class BootcampController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $validated = Validator::make($request->all(), [
+            'title' => 'required',
+            'thumbnail' => 'required|mimes:jpeg,jpg,png',
+            'subtitle' => 'required',
+            'course_category_id' => 'required',
+            'preview_video_link' => 'required|starts_with:https://www.youtube.com/embed/',
+            'description' => 'required',
+            'requirements' => 'required|array|min:1',
+        ])->setAttributeNames([
+            'course_category_id' => 'category',
+            'preview_video_link' => 'video link',
+        ])->validate();
+
+        $course = new Course;
+        $course->course_type_id = 3; //3 seharusnya bootcamp
+        $course->course_category_id = $validated['course_category_id'];
+        $course->thumbnail = Helper::storeImage($request->file('thumbnail'), 'storage/images/bootcamp-courses/');
+        $course->preview_video = $validated['preview_video_link'];
+        $course->title = $validated['title'];
+        $course->subtitle = $validated['subtitle'];
+        $course->description = $validated['description'];
+        $course->save();
+
+        foreach ($request->requirements as $requirement_value) {
+            if ($requirement_value != "") {
+                $new_requirement = new CourseRequirement;
+                $new_requirement->course_id = $course->id;
+                $new_requirement->requirement = $requirement_value;
+                $new_requirement->save();
+            }
+        }
+
+        return redirect()->route('admin.bootcamp.index')->with('message', 'New Online Course has been added!');
     }
 
     /**
@@ -48,7 +92,48 @@ class BootcampController extends Controller
      */
     public function show($id)
     {
-        //
+        $course = Course::findOrFail($id);
+
+        if ($course->courseType->type == 'Bootcamp')
+            return redirect()->route('admin.bootcamp.show', $id); 
+
+        $users = $course->users();
+
+        if ($request->has('sort')) {
+            if ($request['sort'] == "latest") {
+                $users = $users->orderBy('created_at', 'desc');
+            } else {
+                $users = $users->orderBy('created_at');
+            }
+        } else {
+            $users = $users->orderBy('created_at', 'desc');
+        }
+
+        if ($request->has('search')) {
+            if ($request->search == "") {
+                $url = route('admin.bootcamp.show', $id, request()->except('search'));
+                return redirect($url);
+            } else {
+                $search = $request->search;
+
+                $users = $users->where(function ($query) use ($search) {
+                    $query->where([['name', 'like', "%".$search."%"]])
+                    ->orWhere([['email', 'like', "%".$search."%"]]);
+                });
+            }
+        }
+
+        $users = $users->get();
+
+        $total_revenue = 0; $users_data = [];
+        foreach($course->orders as $order) {
+            if ($order->invoice->status == 'paid' || $order->invoice->status == 'completed') {
+                $total_revenue += $order->qty * $order->price;
+                $users_data[$order->invoice->user_id]['invoice_id'] = $order->invoice->id;
+            }
+        }
+
+        return view('admin/bootcamp/detail', compact('course', 'users', 'total_revenue', 'users_data'));
     }
 
     /**
