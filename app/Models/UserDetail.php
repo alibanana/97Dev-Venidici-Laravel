56dd918404449644a304abe05e373ce4b9940352
fernndzaky <fernandhadzaky@hotmail.com>
2021-08-08 10:40:13 +0700

    add response in UserDetail Model

--- app/Models/UserDetail.php
+++ app/Models/UserDetail.php
@@ -16,6 +16,7 @@ class UserDetail extends Model
         'telephone',
         'referral_code',
         'referred_by_code',
+        'response',
         'birthdate',
         'gender',
         'address',
