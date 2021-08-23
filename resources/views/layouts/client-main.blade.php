7a366f2aafb2e09968278a5798b5d8368d419764
Fernandha Dzaky <fernandhadzaky@hotmail.com>
2021-08-23 22:27:36 +0700

    add google tag

--- resources/views/layouts/client-main.blade.php
+++ resources/views/layouts/client-main.blade.php
@@ -28,7 +28,15 @@
     <!-- wow js -->
     <link rel="stylesheet" href="/WOW-master/css/libs/animate.css">
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-LC8WXP5NZZ"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
 
+      gtag('config', 'G-LC8WXP5NZZ');
+    </script>
     
 
     <title>@yield('title')</title>
