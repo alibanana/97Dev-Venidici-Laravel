004714a183e92ebe436e3c6a3b3a5d310803a013
Alifio Rasendriya Rasyid <official.alifio@gmail.com>
2021-08-25 11:37:55 +0700

    Dynamic GAnalytics & FBPixel.
     - Added conditions, only include scripts if the environment is production.

--- resources/views/layouts/client-main.blade.php
+++ resources/views/layouts/client-main.blade.php
@@ -28,15 +28,33 @@
     <!-- wow js -->
     <link rel="stylesheet" href="/WOW-master/css/libs/animate.css">
 
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-LC8WXP5NZZ"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-LC8WXP5NZZ');
-    </script>
+    @env('production')
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-LC8WXP5NZZ"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-LC8WXP5NZZ');
+      </script>
+      <!-- Facebook Pixel Code -->
+      <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '377781967128623');
+        fbq('track', 'PageView');
+      </script>
+      <noscript>
+        <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=377781967128623&ev=PageView&noscript=1"/>
+      </noscript>
+      <!-- End Facebook Pixel Code -->
+    @endenv
     
 
     <title>@yield('title')</title>
